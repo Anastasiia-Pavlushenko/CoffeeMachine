f10f88b6e5a4a907e15d5dfe48189632d7f0420e
Anastasiia Pavlushenko <anastasiia.pavlushenko@nure.ua>
2018-10-04 21:36:36 +0300

    Classes and method

--- Program.cs
+++ Program.cs
@@ -10,6 +10,43 @@ namespace CoffeeMachine
     {
         static void Main(string[] args)
         {
+
         }
     }
+    class CoffeeMachine
+    {
+        public ushort Coffee = 15;
+        public ushort Milk = 10;
+        public ushort Water = 10;
+        public ushort Sugar = 55;
+
+        public ushort MaxOfCoffee = 200;
+        public ushort MaxOfMilk = 200;
+        public ushort MaxOfWater = 200;
+        public ushort MaxOfSugar = 600;
+
+        public uint CashBox = 3250;
+
+        public int PreparationDrink(string TypeOfCoffee, double Price)
+        {
+            return 0;
+        }
+        public int SeveralDrinks(int SumOfDrinks, double Payment)
+        {
+            return 0;
+        }
+        public int Restocking(string TypeOfProducts)
+        {
+        return 0;
+        }
+        public int CashBack()
+        {
+            return 0;
+        }
+        public string Message()
+        {
+            return "Message";
+        }
+    }
+
 }
